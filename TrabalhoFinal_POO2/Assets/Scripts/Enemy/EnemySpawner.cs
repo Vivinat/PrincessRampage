c2d5c8d1534d9777d524df15843bf9354db473c5
Vivinat <venat2044@gmail.com>
2022-10-29 09:25:53 -0300

    Enemy Spawner
    Enemy Spawner Implementado

---
+++ TrabalhoFinal_POO2/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject swarmerPrefab;
+
+    [SerializeField]
+    private float swarmerInterval = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnEnemy(swarmerInterval, swarmerPrefab));
+    }
+
+    private IEnumerator spawnEnemy(float swarmerInterval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(swarmerInterval);  //Qual o intervalo de spawn?
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-1f,1), Random.Range(-2f,2f),0), Quaternion.identity); 
+        //Cria o inimigo novo numa posição determinada pelo Vetor
+        StartCoroutine(spawnEnemy(swarmerInterval, enemy));    //Existe potencial para ser ilimitado
+
+
+    }
+}
